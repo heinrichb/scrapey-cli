258e5d7b0bfadf9e5986663c35b8652dba88d228
dan-avel <danielavelares@gmail.com>
2025-02-13 23:53:46 -0500

    CREATE UTILS PKG

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -5,17 +5,16 @@ package config
 import (
 	"encoding/json"
 	"fmt"
-	"github.com/fatih/color"
 	"os"
-	"reflect"
+	"github.com/heinrichb/scrapey-cli/pkg/utils"
 )
 
 // Config holds configuration data.
 type Config struct {
 	URL string `json:"url,omitempty"`
 	PARSERULES struct {
-		TITLE string `json:title,omitempty`
-		METADESCRIPTION string `json:metaDescription,omitempty`
+		TITLE string `json:"title,omitempty"`
+		METADESCRIPTION string `json:"metaDescription,omitempty"`
 	}
 }
 
@@ -39,56 +38,6 @@ func Load(filePath string) (*Config, error) {
 	if err := json.Unmarshal(content, &cfg); err != nil {
 		return nil, fmt.Errorf("invalid JSON in config file: %v", err)
 	}
-	printNonEmptyFields("", cfg)
+	utils.PrintNonEmptyFields("", cfg)
     return &cfg, nil
-}
-
-// printNonEmptyFields dynamically traverses a struct and prints its non-empty string fields.
-//
-// Parameters:
-// - prefix: A string to prepend to the field name, used to represent nested struct hierarchy (e.g., "Parent.Child.").
-// - v: The struct or pointer to a struct to be traversed and inspected.
-//
-// Usage:
-// This function is useful for dynamically inspecting and displaying configurations or other data structures
-// where the fields may be optional, and only non-empty values are of interest.
-//
-// Example:
-// Given a struct:
-//   type Config struct {
-//       URL string
-//       Nested struct {
-//           Title string
-//       }
-//   }
-// Calling printNonEmptyFields("", configInstance) will output:
-//   URL: http://example.com
-//   Nested.Title: Example Title
-//
-// Notes:
-// - This function relies on the reflect package and assumes that the input is a struct or a pointer to a struct.
-// - Only string fields are checked for non-emptiness; other types are ignored.
-func printNonEmptyFields(prefix string, v interface{}) {
-	val := reflect.ValueOf(v)
-
-	// Handle pointers or nested structs
-	if val.Kind() == reflect.Ptr {
-		val = val.Elem()
-	}
-
-	typ := val.Type()
-
-	// Iterate over fields
-	for i := 0; i < typ.NumField(); i++ {
-		field := val.Field(i)
-		fieldType := typ.Field(i)
-		fieldName := fieldType.Name
-
-		// Handle nested structs
-		if field.Kind() == reflect.Struct {
-			printNonEmptyFields(prefix+fieldName+".", field.Interface())
-		} else if field.Kind() == reflect.String && field.String() != "" {
-			fmt.Println(color.New(color.FgHiYellow).Sprint(prefix+fieldName+":"), field.String())
-		}
-	}
 }
